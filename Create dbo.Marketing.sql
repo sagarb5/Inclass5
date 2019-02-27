983a8c82f31f4cb7f1cabbd843642514d96d193e
sagarb5 <46499836+sagarb5@users.noreply.github.com>
2019-02-26 21:29:43 -0500

    Add files via upload

---
+++ Create dbo.Marketing.sql
@@ -0,0 +1,23 @@
+﻿USE [Products]
+GO
+
+/****** Object: Table [dbo].[Marketing] Script Date: 2/26/2019 9:16:33 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Marketing]
+(
+	[Id] INT NOT NULL , 
+    [Batch] VARCHAR(50) NOT NULL , 
+    [Year] VARCHAR(50) NULL, 
+    [Quarter] INT NOT NULL, 
+    [Budget] INT NULL, 
+    [ProductId] VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES DBO.Products(ProductId), 
+    PRIMARY KEY ([ProductId])
+)
+
+
+
---
+++ Create dbo.Products.sql
@@ -0,0 +1,21 @@
+﻿USE [Products]
+GO
+
+/****** Object: Table [dbo].[Products] Script Date: 2/26/2019 9:17:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products] (
+    [Id]          INT          NOT NULL,
+    [ProductName] VARCHAR (50) NULL,
+    [Category]    VARCHAR (50) NULL,
+    [Color]       VARCHAR (50) NULL,
+    [Batch]       VARCHAR (50) NULL,
+    [Year]        VARCHAR (50) NULL,
+    [ProductId]   VARCHAR (50) NOT NULL
+);
+
+
